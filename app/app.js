f9fd924a32d54d110be0c1fbe30198e2eaefa899
jazmin10 <jazminestrada10@gmail.com>
2018-08-06 18:17:03 -0500

    rendered basic html to the page

---
+++ app/app.js
@@ -0,0 +1,6 @@
+// ============= GLOBAL VARIABLES =============
+	import React from "react";
+	import ReactDOM from "react-dom";
+
+// ============= MAIN PROCESSES =============
+	ReactDOM.render(<h1>IT WORKED!!!!</h1>, document.getElementById("app"));
