6c18b95211016ab559633387878c1226009f1c83
jazmin10 <jazminestrada10@gmail.com>
2018-08-06 17:44:35 -0500

    creating db connection and starting server

---
+++ server.js
@@ -0,0 +1,32 @@
+// ============= GLOBAL VARIABLES =============
+
+	// Import modules
+	const express = require("express");
+	const bodyParser = require("body-parser");
+	const mongoose = require("mongoose");
+
+	mongoose.connect("mongodb://localhost/reactdb");
+	const PORT = process.env.PORT || 3000;
+	const app = express();
+	const db = mongoose.connection;
+
+	// Middleware
+	app.use(bodyParser.json());
+	app.use(bodyParser.urlencoded({ extended: true }));
+	app.use(bodyParser.text());
+	app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+	app.use(express.static("public"));
+
+// ============= MAIN PROCESSES =============
+
+	// If a database connection error occurs...
+	db.on("error", (err) => {
+		console.log("database error:", err);
+	});
+
+	// When the connection to the database is successful, start the server
+	db.once("open", () => {
+		app.listen(PORT, () => {
+			console.log("app listening on PORT:", PORT);
+		});
+	});
