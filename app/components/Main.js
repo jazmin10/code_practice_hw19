63f6fb572a09c41907a7a96ed5bda07b9378fadc
jazmin10 <jazminestrada10@gmail.com>
2018-08-06 19:32:21 -0500

    added navbar to Main component

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,19 +1,43 @@
 // ============= GLOBAL VARIABLES =============
-	import React from 'react';
+	import React from "react";
+	import {Link} from "react-router";
 
 // ============= MAIN PROCESSES =============
 	export default class Main extends React.Component {
 		render() {
 			return (
-				<div className="container">
 
-					{/* JUMBOTRON */}
-					<div className="jumbotron text-center">
-					  <h1 className="display-4">New York Times Article Scrubber</h1>
-					  <p className="lead">Search for and annotate articles of interest!</p>
+				<div>
+
+					{/* NAVBAR */}
+					<nav className="navbar navbar-expand-lg navbar-light bg-light">
+					  <Link to="/" className="navbar-brand">Article Scrubber</Link>
+					  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+					    <span className="navbar-toggler-icon"></span>
+					  </button>
+
+					  <div className="collapse navbar-collapse" id="navbarSupportedContent">
+					    <ul className="navbar-nav mr-auto">
+					      <li className="nav-item">
+					        <Link to="/search" className="nav-link">Search</Link>
+					      </li>
+					      <li className="nav-item">
+					        <Link to="/saved" className="nav-link">Saved</Link>
+					      </li>
+					    </ul>
+					  </div>
+					</nav>
+
+					<div className="container">
+
+						{/* JUMBOTRON */}
+						<div className="jumbotron text-center">
+						  <h1 className="display-4">New York Times Article Scrubber</h1>
+						  <p className="lead">Search for and annotate articles of interest!</p>
+						</div>
+
+						{this.props.children}
 					</div>
-
-					{this.props.children}
 				</div>
 			);
 		}
