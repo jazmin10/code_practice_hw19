e74a93ea2371e061b45dbd110914e1b35c19b960
jazmin10 <jazminestrada10@gmail.com>
2018-08-06 19:19:28 -0500

    created basic structure for Saved component

--- app/config/routes.js
+++ app/config/routes.js
@@ -4,12 +4,14 @@
 	import router, {Route, Router, hashHistory, IndexRoute} from "react-router";
 	import Main from "../components/Main.js";
 	import Search from "../components/Search.js";
+	import Saved from "../components/Saved.js";
 
 // ============= MAIN PROCESSES =============
 	export default (
 		<Router history={hashHistory}>
 			<Route path="/" component={Main}>
 				<Route path="search" component={Search} />
+				<Route path="saved" component={Saved} />
 				<IndexRoute component={Search} />
 			</Route>
 		</Router>
